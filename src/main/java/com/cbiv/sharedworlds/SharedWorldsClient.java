48fd2db71b04fa18e95ccaf5dc549b14f650ed27
Justin McDonald <MCDJustinAndrew@outlook.com>
2025-12-25 15:09:59 -0500

    Initial Commit

---
+++ src/main/java/com/cbiv/sharedworlds/SharedWorldsClient.java
@@ -0,0 +1,9 @@
+package com.cbiv.sharedworlds;
+
+import net.fabricmc.api.ClientModInitializer;
+public class SharedWorldsClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
